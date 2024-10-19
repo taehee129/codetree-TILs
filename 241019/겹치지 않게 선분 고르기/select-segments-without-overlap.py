3945c7ffccfcc6efe824b5b364f7f14708e5e7de
taehee129 <89380104+taehee129@users.noreply.github.com>
2024-10-19 15:56:28 +0900

    Title: 겹치지 않게 선분 고르기, Time: 182ms, Memory: 26MB, Status: Passed - CodeTree

--- 241019/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 241019/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -7,21 +7,21 @@ line =[
 ]
 answer=0
 
-print(line)
 
-def check(lst, obj) :
-    for i in lst :
-        if (line[i][0] <= obj[0] and line[i][1] >= obj[0]) or (line[i][0] <= obj[1] and line[i][1] >= obj[1]) :
+def check(lst, obj):
+    for i in lst:
+        # Check if there is any overlap between two intervals
+        if not (line[i][1] < obj[0] or line[i][0] > obj[1]):
             return False
     return True
-    
-
 
 def choose(index, lst) :
     global answer
-
+  
     if index== n : 
+        
         answer = max(answer,len(lst))
+
 
         return
 
@@ -29,9 +29,10 @@ def choose(index, lst) :
         flag = check(lst,line[i])
         if flag:
             lst.append(i)
-        lst = list(lst)
+        
         choose(i+1,lst)
-
+        if flag :
+            lst.pop()
         
 
 
