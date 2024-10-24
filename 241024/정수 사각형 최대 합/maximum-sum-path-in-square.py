004060cac544d3ceedae9311ea1f06258e9e889d
taehee129 <89380104+taehee129@users.noreply.github.com>
2024-10-24 10:46:05 +0900

    Title: 정수 사각형 최대 합, Time: 169ms, Memory: 25MB, Status: Passed - CodeTree

--- 241024/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 241024/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -20,7 +20,7 @@ for i in range(1,n) :
 for i in range(1,n) :
     dp[0][i] = dp[0][i-1] + lst[0][i]
 
-answer = 0
+answer = dp[0][0]
 for i in range(1,n) :
     for j in range(1,n) :
         dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + lst[i][j]
