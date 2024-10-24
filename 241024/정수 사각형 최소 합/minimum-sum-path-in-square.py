12bbdfe989adf80b01547f260114a50b0fec31ee
taehee129 <89380104+taehee129@users.noreply.github.com>
2024-10-24 11:02:30 +0900

    Title: 정수 사각형 최소 합, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241024/정수 사각형 최소 합/minimum-sum-path-in-square.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+lst = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+
+dp = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+
+dp[0][n-1] = lst[0][n-1]
+
+
+for i in range(n-2,-1,-1) :
+    dp[0][i] = dp[0][i+1]+lst[0][i]
+
+for i in range(1,n) : 
+    dp[i][n-1] = dp[i-1][n-1] +lst[i][n-1]
+
+
+for i in range(1,n) :
+    for j in range(n-2,-1,-1) :
+        dp[i][j] = min(dp[i-1][j], dp[i][j+1]) + lst[i][j]
+
+
+print(dp[n-1][0])
