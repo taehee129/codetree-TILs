866370366ecca0f171902d052f3a6b33ff587547
taehee129 <89380104+taehee129@users.noreply.github.com>
2024-10-29 10:28:59 +0900

    Title: 동전 거슬러주기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241029/동전 거슬러주기/coin-change.py
+++ 241029/동전 거슬러주기/coin-change.py
@@ -3,18 +3,16 @@ n,m = tuple(map(int,input().split()))
 lst = list(map(int,input().split()))
 
 
-dp = [10001]*m 
-for i in lst :
-    if i-1 <m :
-        dp[i-1] = 1
+dp = [10001]*(m+1) 
 
-for i in range(m) :
+dp[0] =1 
+for i in range(1,m+1) :
     for j in range(n) :
         if i-lst[j] >= 0 :
             dp[i] = min(dp[i],dp[i-lst[j]]+1)
 
 
-if dp[m-1] ==10001 :
+if dp[m] ==10001 :
     print(-1)
 else :
-    print(dp[m-1])
+    print(dp[m])
