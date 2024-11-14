8f950cf36532312951778d62b28fb735c21785f0
taehee129 <89380104+taehee129@users.noreply.github.com>
2024-11-14 22:35:42 +0900

    Title: 자연수 M/2개의 쌍, Time: 4258ms, Memory: 36MB, Status: Passed - CodeTree

--- 241114/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 241114/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -5,18 +5,21 @@ lst = [
     for _ in range(n)
 ]
 
+lst.sort(key=lambda x:x[1],reverse=True)
+
+
 
 m=0
 for i in lst :
     m +=i[0]
 
 maxVal =0
-x = 0
+x= 0
 y=n-1
 for i in range(int(m/2)) :
 
     if lst[x][0] <1 :
-        x+=1
+        x +=1
     if lst[y][0] <1 :
         y -=1
     lst[x][0] -=1
