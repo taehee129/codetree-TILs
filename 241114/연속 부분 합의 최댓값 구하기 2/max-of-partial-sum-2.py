602250fe2a5772bf41ad345bc149bd2ae1504f28
taehee129 <89380104+taehee129@users.noreply.github.com>
2024-11-14 21:41:13 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 50ms, Memory: 18MB, Status: Passed - CodeTree

--- 241114/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 241114/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -2,13 +2,15 @@ n= int(input())
 
 lst = list(map(int,input().split()))
 
-maxSum=0
+
+maxSum = -1000
 sumVal =0
 for val in lst :
     sumVal += val
     maxSum = max(maxSum, sumVal)
     if sumVal < 0 :
         sumVal=0
+
 
 
 print(maxSum)
