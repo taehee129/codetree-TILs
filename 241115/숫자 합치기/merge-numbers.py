969540544c2d46ab9d81e17f5128bac996792594
taehee129 <89380104+taehee129@users.noreply.github.com>
2024-11-15 11:01:34 +0900

    Title: 숫자 합치기, Time: 5054ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 241115/숫자 합치기/merge-numbers.py
+++ 241115/숫자 합치기/merge-numbers.py
@@ -10,8 +10,13 @@ while len(lst) >1 :
     val = lst[0]+lst[1]
     sumVal += lst[0]+lst[1]
     lst = lst[2:]
-    lst.append(val)
-    lst.sort()
-
-
+    for i in range(len(lst)) :
+        if val <= lst[i] :
+            lst.insert(i,val)
+            break
+        if i == len(lst)-1 :
+            lst.append(val)
+    
 print(sumVal)
+
+
