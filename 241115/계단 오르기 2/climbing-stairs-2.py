70b960224945ec7651505cfb08985d9b6429e87d
taehee129 <89380104+taehee129@users.noreply.github.com>
2024-11-15 15:43:59 +0900

    Title: 계단 오르기 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241115/계단 오르기 2/climbing-stairs-2.py
+++ 241115/계단 오르기 2/climbing-stairs-2.py
@@ -7,23 +7,26 @@ dp = [[0,0,0,0]
     for _ in range(n)
 ]
 
-dp[0]= [0,lst[0],0]
-dp[1] = [lst[1], 0, lst[0]+lst[1]]
+dp[0]= [0,lst[0],0,0]
+dp[1] = [lst[1], 0, lst[0]+lst[1],0]
 for i in range(2,n) :
     val = lst[i]
-    # dp[i] = [max(dp[i-2])+val,dp[i-1][0]+val,dp[i-1][1]+val,dp[i-1][2]+val]
-    dp[i][0] = max(dp[i-2])+val
+    
+    dp[i][0] = dp[i-2][0]+val
 
-    for j in range(2) :
-        if dp[i-1][j] != 0 :
-           
-            dp[i][j+1] = dp[i-1][j]+val
+    for j in range(3) :
+        maxVal = max(dp[i-1][j],dp[i-2][j+1])
+        if maxVal != 0 :
+            dp[i][j+1] =maxVal+val
 
-
+    # dp[i][1] = max(dp[i-1][0],dp[i-2][1])+val
+    # dp[i][2] = max(dp[i-1][1],dp[i-2][2])+val
+    # dp[i][3] = max(dp[i-1][2],dp[i-2][3])+val
 
 
 
 print(max(dp[n-1]))
 
+# dp
 
-371196
+
