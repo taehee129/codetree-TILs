5fceeba5051f6d74a473e1ac121d544069cdea6d
taehee129 <89380104+taehee129@users.noreply.github.com>
2024-11-15 15:12:57 +0900

    Title: 계단 오르기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241115/계단 오르기 2/climbing-stairs-2.py
+++ 241115/계단 오르기 2/climbing-stairs-2.py
@@ -3,16 +3,27 @@ n = int(input())
 lst = list(map(int,input().split()))
 
 
-dp = [[0,0,0]]*n
+dp = [[0,0,0,0] 
+    for _ in range(n)
+]
 
-dp[0]= [lst[0],0,0]
-
-
-for i in range(n) :
+dp[0]= [0,lst[0],0,0]
+dp[1] = [lst[1], 0, lst[0]+lst[1],0]
+for i in range(2,n) :
     val = lst[i]
-    dp[i] = [max(dp[i-2])+val,dp[i-1][0]+val,dp[i-1][1]+val]
+    # dp[i] = [max(dp[i-2])+val,dp[i-1][0]+val,dp[i-1][1]+val,dp[i-1][2]+val]
+    dp[i][0] = max(dp[i-2])+val
+
+    for j in range(3) :
+        if dp[i-1][j] != 0 :
+           
+            dp[i][j+1] = dp[i-1][j]+val
+
+
+
 
 
 print(max(dp[n-1]))
 
 
+371196
