286e5ecf8d7af6dc0064a7ce06326e2c34f6a6af
taehee129 <89380104+taehee129@users.noreply.github.com>
2024-11-22 15:43:39 +0900

    Title: 수정 수집하기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241122/수정 수집하기/collect-crystals.py
+++ 241122/수정 수집하기/collect-crystals.py
@@ -6,12 +6,14 @@ dp =[[0 for _ in range(k+1)] for _ in range(n)]
 
 if lst[0] == "L" :
     dp[0][0] = 1
+else :
+    dp[0][1] = 1
+
 
 # k%2==0 L  k%2== 1 R
 for i in range(1,n) :
     for j in range(k+1) :
-        if i<j :
-            continue
+
         if j == 0 :
             if lst[i] == 'L' :
                 dp[i][j] = dp[i-1][j]+1
@@ -21,5 +23,5 @@ for i in range(1,n) :
             dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) 
             if (j%2 == 0 and lst[i] == "L" ) or (j%2 == 1 and lst[i]=="R") :
                 dp[i][j] +=1
-
+    print(dp)
 print(max(dp[n-1]))
