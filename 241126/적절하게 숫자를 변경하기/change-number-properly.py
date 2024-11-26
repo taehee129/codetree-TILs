b4bea9cb3427e9219392fa1ad0771d94537773ac
taehee129 <89380104+taehee129@users.noreply.github.com>
2024-11-26 10:38:22 +0900

    Title: 적절하게 숫자를 변경하기, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241126/적절하게 숫자를 변경하기/change-number-properly.py
@@ -0,0 +1,44 @@
+n,m = tuple(map(int,input().split()))
+
+lst= list(map(int,input().split()))
+m+=1
+dp = [
+       [
+            [0            
+            for _ in range(4)            
+            ] 
+        for _ in range(m)
+        ]
+    for _ in range(n)
+    ]
+
+
+dp[0][0][lst[0]-1] =1 
+
+
+# m 다른 횟수 
+#dp[n][m][k] = 1.dp[n-1][m] or 2.dp[n-1][m-1]
+#           1. dp[n-1][m][k] lst[n] =k 라면 +1 
+#           2. dp[n-1][m-1][~k] lst[n]= ~k 라면 +1 중에서 최대값 
+
+answer = 0
+for i in range(1,n) :
+    for j in range(m) :
+        for k in range(4) :
+            maxVal = 0
+            maxVal = max(maxVal, dp[i-1][j][k]+ (1 if (k+1) ==lst[i] else 0))
+
+            for x in range(4) :
+                if j ==0 :
+                    break
+                if x ==k :
+                    continue 
+                maxVal =max(maxVal, dp[i-1][j-1][x] +(1 if (k+1)==lst[i] else 0))
+        
+            dp[i][j][k] = maxVal
+            answer = max(answer,maxVal)
+        
+
+
+print(answer)
+
