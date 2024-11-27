5d14ebf3e265541d19d8ba7e26faaf3786d9fdff
taehee129 <89380104+taehee129@users.noreply.github.com>
2024-11-27 13:54:33 +0900

    Title: 최장 공통 부분 수열, Time: 82ms, Memory: 20MB, Status: Runtime Error - CodeTree

---
+++ 241127/최장 공통 부분 수열/longest-common-sequence.py
@@ -0,0 +1,35 @@
+a= input()
+b= input() 
+
+
+lenA= len(a)
+lenB = len(b)
+dp = [[0 for _ in range(lenB)] for _ in range(lenA)]
+
+
+if a[0] == b[0] : 
+    dp[0][0] = 1
+
+
+for i in range(1,lenA) : 
+    if b[0] == a[i] :
+        dp[i][0] = 1
+    else :
+        dp[i][0] = dp[i-1][0]
+
+for i in range(1,lenB) :
+    if a[0] == b[i] :
+        dp[0][i] = 1 
+    else :
+        dp[0][i] = dp[0][i-1]
+
+
+for i in range(1,lenA) :
+    for j in range(1,lenB) :
+        if a[i] == b[j] :
+            dp[i][j] = dp[i-1][j-1] +1 
+        else :
+            dp[i][j] = max(dp[i][j-1], dp[j-1][i])
+
+
+print(dp[lenA-1][lenB-1])
