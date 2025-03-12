0df525ecd19f43a77c58c28b678890c2273311be
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-03-12 16:47:50 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 5012ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

---
+++ 250312/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,41 @@
+n,m = tuple(map(int,input().split()))
+
+
+grid = [list(map(int,input().split())) for _ in range(n)]
+
+
+def in_range(x,y) : 
+    if x<0 or x>=n or y<0 or y>=m : 
+        return False
+    return True 
+
+flag =0
+visited = [[0 for _ in range(m)] for _ in range(n)]
+
+q = [(0,0)]
+
+dxs, dys = [1,0,-1,0],[0,1,0,-1]
+while len(q) >0 : 
+    
+    x,y = q.pop()
+    if (x,y) == (n-1,m-1) : 
+        flag = 1 
+        break
+    
+    visited[x][y] = 1
+
+    for dx,dy in zip(dxs,dys) : 
+
+        newx,newy = x+dx,y+dy
+
+        if not in_range(newx,newy) : 
+            continue 
+        if grid[newx][newy] == 0 :
+            continue 
+        if visited[newx][newy] : 
+            continue
+
+        q.insert(0,(newx,newy))
+
+
+print(flag)
