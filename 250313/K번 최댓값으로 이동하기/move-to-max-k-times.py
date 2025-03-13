3a93334ff0e3d4c6087ebad65be94c51de16d505
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-03-13 17:03:46 +0900

    Title: K번 최댓값으로 이동하기, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250313/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -0,0 +1,51 @@
+n,k = tuple(map(int,input().split()))
+
+grid = [list(map(int,input().split())) for _ in range(n)]
+
+x,y = tuple(map(int,input().split()))
+x,y = x-1,y-1
+
+def in_range(x,y) : 
+    if x<0 or x>=n or y<0 or y>=n :
+        return False
+    return True
+def bfs(x,y) : 
+    q = [(x,y)]
+    sx,sy = x,y
+    dxs ,dys = [1,0,-1,0],[0,1,0,-1]
+    maxVal = 0 
+    rx,ry = (x,y)
+
+    while len(q) >0 : 
+
+        x,y = q.pop()
+        #print(x,y)
+        for dx,dy in zip(dxs,dys) : 
+            nx,ny = x+dx,y+dy 
+            
+            if not in_range(nx,ny) :
+                continue 
+            if visited[nx][ny] == 1 :
+                continue 
+            if grid[nx][ny] >= grid[sx][sy] :
+                continue
+            
+            q.insert(0,(nx,ny))
+            visited[nx][ny] = 1 
+            
+            if maxVal < grid[nx][ny] :
+                rx,ry = nx,ny 
+                maxVal = grid[nx][ny]
+            elif maxVal == grid[nx][ny] : 
+                if nx < rx : 
+                    rx,ry = nx,ny
+                elif nx == rx and ny < ry : 
+                    rx,ry = nx,ny
+             
+    return rx,ry 
+
+for _ in range(k) :
+    visited= [[0 for _ in range(n)] for _ in range(n)] 
+    x,y = bfs(x,y)
+print(x+1,y+1)            
+
