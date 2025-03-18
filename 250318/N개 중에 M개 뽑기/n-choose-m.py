984a1ef39f99bfb98c7f69cd8d1652c01f3504f3
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-03-18 16:09:13 +0900

    Title: N개 중에 M개 뽑기, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250318/N개 중에 M개 뽑기/n-choose-m.py
+++ 250318/N개 중에 M개 뽑기/n-choose-m.py
@@ -3,34 +3,20 @@ n,m = map(int,input().split())
 nums = [i for i in range(1,n+1)]
 
 
-combination = []
+answer_list =[]
 
-def print_num() :
-    answer_list = []
-    for i in range(len(combination)):
-        if combination[i] !=0 :
-            answer_list.append(nums[i])
-    
-    print(' '.join(map(str,answer_list)))
-            
+def choose(idx) : 
+    if len(answer_list) == m :
+        print(' '.join(map(str,answer_list)))
 
-def choose(num,cnt) :
-    if cnt == m : 
-        print_num()
-        return 
-    
-    if num == n : 
-        return 
-    
-    combination.append(1)
-    choose(num+1,cnt+1)
-    combination.pop()
+    for i in range(idx,n):
+        answer_list.append(nums[i])
+        choose(i+1)
+        answer_list.pop()
 
-    combination.append(0)
-    choose(num+1,cnt)
-    combination.pop()
 
-choose(0,0)
-
-    
+for i in range(0,n) :
+    answer_list.append(nums[i])
+    choose(i+1)
+    answer_list.pop()
     
