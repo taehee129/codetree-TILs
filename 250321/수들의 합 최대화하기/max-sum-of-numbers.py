2a4f7fc6fbeb5ca6919053c6a0e083bad8d68a54
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-03-21 11:31:26 +0900

    Title: 수들의 합 최대화하기, Time: 95ms, Memory: 20MB, Status: Passed - CodeTree

--- 250321/수들의 합 최대화하기/max-sum-of-numbers.py
+++ 250321/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -1,40 +1,34 @@
-n= int(input())
+n = int(input())
 grid = [list(map(int,input().split())) for _ in range(n)]
-
 
 selected = []
 
-rows = [False]*n
-cols = [False]*n 
+rows_visited = [False]*n 
 
 max_val = 0
 
-def permutation() :
+def permutation(col) : 
     global max_val
-    if len(selected) == n :
-        val = 0 
+    if col == n : 
+        val = 0
         for x,y in selected :
-            val += grid[x][y]
+            val+= grid[x][y]
+            max_val = max(max_val,val)
 
-        max_val = max(max_val,val)
-    
-        return 
-    
+        return
+  
     for i in range(n) :
-        for j in range(n) : 
-            if rows[i] or cols[j] :                
-                continue 
-            
-            selected.append((i,j))
-            rows[i] = True
-            cols[j] = True 
+        if rows_visited[i] :
+            continue 
+        
+        selected.append((i,col))
+        rows_visited[i] = True 
 
-            permutation()
+        permutation(col+1)
 
-            selected.pop()
-            rows[i] = False
-            cols[j] = False
+        selected.pop()
+        rows_visited[i] = False
 
-permutation()
+
+permutation(0)
 print(max_val)
-
