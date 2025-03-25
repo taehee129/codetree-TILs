ba0760ef1bea2a0ccf3c095b932b112c14a13e26
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-03-25 13:51:52 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 107ms, Memory: 33MB, Status: Runtime Error - CodeTree

--- 250325/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 250325/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -8,28 +8,37 @@ def op(num,op) :
     elif op == 1 :
         return num+1
     elif op ==2 :
-        return num/2 
+        return int(num/2) 
     elif op ==3 :
-        return num/3
+        return int(num/3)
 
+visited = [False]*1000000
 
 def bfs(num) :
 
     q = [(num,0)]
-
+    visited[num-1] = True
     while len(q) >= 1 :
 
         num,idx = q.pop()
+        #print(num,idx)
         if num ==1 :
             return idx
 
         for i in range(4) :
+            
             if i == 2 and num %2 !=0 :
                 continue 
             if i ==3 and num %3 != 0 :
                 continue 
             
-            q.insert(0,(op(num,i),idx+1))
+            new_num = op(num,i)
+            
+            if visited[new_num-1] : 
+                continue
+
+            q.insert(0,(new_num,idx+1))
+            visited[new_num-1] = True
         
 
 print(bfs(n))
