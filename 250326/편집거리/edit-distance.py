56797614a6c4de13b57de19a18f9e9fb2181b876
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-03-26 17:24:24 +0900

    Title: 편집거리, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

--- 250326/편집거리/edit-distance.py
+++ 250326/편집거리/edit-distance.py
@@ -29,7 +29,9 @@ for i in range(1,n) :
         if a[i] == b[j] : 
             dp[i][j] = dp[i-1][j-1]
         else : 
-            dp[i][j] = min(dp[i-1][j],dp[i][j-1]) +1
+            
+            dp[i][j] = min(dp[i-1][j]+1,dp[i][j-1]+1, dp[i-1][j-1]+1)
+
 
 
 
