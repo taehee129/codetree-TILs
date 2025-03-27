db3f5d479e91d3043dbd9ed162ed74770c853b12
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-03-27 15:48:39 +0900

    Title: 최장 공통 부분 수열 2, Time: 84ms, Memory: 24MB, Status: Wrong Answer - Codetree

---
+++ 250327/최장 공통 부분 수열 2/longest-common-sequence-2.py
@@ -0,0 +1,33 @@
+a = input()
+b= input()
+
+n,m = len(a), len(b)
+
+dp = [['' for _ in range(m)] for _ in range(n)]
+
+
+if a[0] == b[0] : 
+    dp[0][0] += a[0]
+
+
+for i in range(1,m) : 
+    dp[0][i] = dp[0][i-1]
+
+    if a[0] == b[i] :
+        dp[0][i] = a[0]
+
+for i in range(1,n) : 
+    dp[i][0] = dp[i][0]
+
+    if a[i] == b[0] :
+        dp[i][0] = a[i]
+
+for i in range(1,n) : 
+    for j in range(1,m) : 
+        if a[i] == b[j] : 
+            dp[i][j] = dp[i-1][j-1] + a[i]
+        else : 
+            dp[i][j] = dp[i-1][j] if len(dp[i-1][j]) > len(dp[i][j-1]) else dp[i][j-1]
+
+
+print(dp[n-1][m-1])
