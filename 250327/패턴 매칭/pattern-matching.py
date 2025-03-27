2ae37d30675cbf5b0d8fa0a02130228ebc340528
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-03-27 14:42:37 +0900

    Title: 패턴 매칭, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250327/패턴 매칭/pattern-matching.py
+++ 250327/패턴 매칭/pattern-matching.py
@@ -10,19 +10,24 @@ if p[0] == '.' or p[0] == s[0] :
     dp[0][0] = True 
 
 if len(p) >=2 and p[1] =='*' :
-    dp[0][1] = True
+    dp[0][1] = dp[0][0]
 
 
 for i in range(1,n) :
     for j in range(1,m) :
-        if p[j] == '*' and dp[i-1][j] == True and s[i] == s[n-1]  :
-            dp[i][j] = True 
+        if p[j] == '*' :
+            if (s[i] == p[j-1] or p[j-1] =='.') and dp[i-1][j] == True : 
+                dp[i][j] = True 
+            if s[i] != p[j-1] and dp[i][j-2] == True:
+                dp[i][j]= True
+
+
         elif p[j] == '.' and dp[i-1][j-1] == True :
             dp[i][j] = True 
         elif p[j] == s[i] and dp[i-1][j-1] == True :
             dp[i][j] = True
          
-if dp[n-1][m-1] or p=='.*':
+if dp[n-1][m-1]:
     print('true')
 else : 
     print('false')
