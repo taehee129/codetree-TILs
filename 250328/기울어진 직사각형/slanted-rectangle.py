99177940856de360317982183102ccf26a5ab360
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-03-28 17:02:47 +0900

    Title: 기울어진 직사각형, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250328/기울어진 직사각형/slanted-rectangle.py
@@ -0,0 +1,51 @@
+n = int(input())
+grid = [list(map(int,input().split())) for _ in range(n)]
+
+dxy = [(-1,1),(-1,-1),(1,-1),(1,1)]
+
+x,y =1,1 
+
+def in_range(x,y) : 
+    if x<0 or x>=n or y<0 or y>=n : 
+        return False
+    return True
+    
+def rect(r,c,x,y) : 
+    sum_val = 0
+    for i in range(x) : 
+        r,c = r-1,c+1
+        if not in_range(r,c) :
+            return False
+        sum_val += grid[r][c]
+
+    for i in range(y) : 
+        r,c = r-1,c-1
+        if not in_range(r,c) :
+            return False
+        sum_val += grid[r][c]
+    for i in range(x) : 
+        r,c = r+1,c-1
+        if not in_range(r,c) :
+            return False
+        sum_val += grid[r][c]
+    for i in range(y) : 
+        r,c = r+1,c+1
+        if not in_range(r,c) :
+            return False
+        sum_val += grid[r][c]
+    return sum_val 
+
+
+
+max_val =0
+for i in range(n) :
+    for j in range(n) : 
+        for x in range(n) :
+            for y in range(n) : 
+                val = rect(i,j,x,y)
+                if val :
+                    max_val = max(val,max_val)
+                else :
+                    break
+        
+print(max_val)
