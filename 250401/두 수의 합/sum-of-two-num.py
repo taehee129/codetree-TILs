3a436c7a0c03adc90296a838f76c8c57b96890a0
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-04-01 10:41:07 +0900

    Title: 두 수의 합, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250401/두 수의 합/sum-of-two-num.py
+++ 250401/두 수의 합/sum-of-two-num.py
@@ -3,19 +3,11 @@ n,k = map(int,input().split())
 
 nums = list(map(int,input().split()))
 
-com = []
-end = len(nums)
-cnt = 0
-def combination(idx) :
-    global cnt
-    if len(com) == 2 :
-        if com[0] + com[1] == k:
-            cnt+=1
-    
-    for i in range(idx,end) : 
-        com.append(nums[i])
-        combination(i+1)
-        com.pop()
-     
-combination(0)
+cnt =0
+for i in range(n) :
+    for j in range(i,n) :
+        if nums[i] +nums[j] == k :
+            cnt +=1
+
+
 print(cnt)
