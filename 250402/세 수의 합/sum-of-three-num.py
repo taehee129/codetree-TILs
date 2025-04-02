f2a7bb8e96293232e9b6abb5a82c9d19958ff035
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-04-02 11:32:22 +0900

    Title: 세 수의 합, Time: 67ms, Memory: 17MB, Status: Passed - Codetree

--- 250402/세 수의 합/sum-of-three-num.py
+++ 250402/세 수의 합/sum-of-three-num.py
@@ -1,29 +1,30 @@
-n,k = map(int,input().split())
+# 변수 선언 및 입력:
+n, k = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-nums = list(map(int,input().split()))
+count = dict()
 
-def two(n,k) : 
-    
-    nums2 = nums[0:n]
-    d = {} 
-    ret = 0
-    for i in range(n) : 
-        num = nums2[i]   
-        if k-num in d : 
-            ret += d[k-num]
-        
-        if num in d :
-            d[num] +=1 
-        else :
-            d[num] = 1 
-    
-    return ret 
+# 각 숫자가 몇 번씩 나왔는지를
+# hashmap에 기록해줍니다.
+for elem in arr:
+    if elem in count:
+        count[elem] += 1
+    else:
+        count[elem] = 1
 
+ans = 0
+# 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
+for i in range(n):
+    # 이미 순회한 적이 있는 숫자는 빼 버림으로서
+    # 같은 조합이 여러번 세어지는 걸 방지합니다.
+    count[arr[i]] -= 1
 
-asw = 0
-for i in range(2,n) : 
-    asw += two(i, k-nums[i])
-    
+    for j in range(i):
+        # 전처리를 해주었기 때문에 이미 순회한 적 있는 값은 hashmap에 없습니다.
+        # 이와 같은 방식으로 같은 조합이 중복되어 세어지는 걸 방지할 수 있습니다.
+        diff = k - arr[i] - arr[j]
 
-print(asw)
+        if diff in count:
+            ans += count[diff]
 
+print(ans)
