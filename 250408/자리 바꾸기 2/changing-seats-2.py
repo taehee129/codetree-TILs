afccc661bbf09ea60e03e3370dee267fe26ad6de
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-04-08 12:58:50 +0900

    Title: 자리 바꾸기 2, Time: 162ms, Memory: 29MB, Status: Passed - Codetree

--- 250408/자리 바꾸기 2/changing-seats-2.py
+++ 250408/자리 바꾸기 2/changing-seats-2.py
@@ -5,8 +5,8 @@ lst = [i for i in range(n)]
 
 def swap(a,b) : 
     lst[a],lst[b] = lst[b], lst[a]
-    set_lst[lst[a]].add(b)
-    set_lst[lst[b]].add(a)
+    set_lst[lst[b]].add(b)
+    set_lst[lst[a]].add(a)
 
 
 swap_lst = [tuple(map(int,input().split())) for _ in range(k)]
@@ -14,7 +14,7 @@ swap_lst = [tuple(map(int,input().split())) for _ in range(k)]
 for i in range(3*k) : 
     a, b = swap_lst[i%k]
     swap(a-1,b-1)
-    
+ 
 
 for s in set_lst : 
     print(len(s))
