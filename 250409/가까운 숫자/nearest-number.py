ce51bfabcc2baa5873d47fa7b9872c70ada14ac8
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-04-09 14:30:55 +0900

    Title: 가까운 숫자, Time: 997ms, Memory: 36MB, Status: Wrong Answer - Codetree

---
+++ 250409/가까운 숫자/nearest-number.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+nums = list(map(int,input().split()))
+
+min_dis = 1000000
+
+from sortedcontainers import SortedSet 
+
+s = SortedSet() 
+s.add(0)
+
+for num in nums : 
+    idx = s.bisect_right(num)
+
+        
+    if idx != len(s) :
+        min_dis = min(min_dis, s[idx]-num)
+    
+    min_dis = min(min_dis,abs(s[idx-1]-num))
+    print(min_dis)
+    s.add(num)
+
+
+
+
