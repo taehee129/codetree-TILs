e2858ab16df4bceb02569909dd37326f5e311c92
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-04-10 16:06:33 +0900

    Title: 자리 차지하기, Time: 115ms, Memory: 23MB, Status: Wrong Answer - Codetree

--- 250410/자리 차지하기/take-place.py
+++ 250410/자리 차지하기/take-place.py
@@ -3,15 +3,18 @@ from sortedcontainers import SortedSet
 
 n,m = map(int,input().split())
 
-nums =  SortedSet(map(int,input().split()))
+nums =  list(map(int,input().split()))
+nums.sort()
 
 # 빈 자리이면서 앉을 수 있는 자리의 최댓값 
 
 cnt = 0
+end = len(nums)-1
 for i in range(m,0,-1) : 
-    if len(nums)>0 and nums[-1] >= i :
+    if nums[end] >= i :
         cnt +=1 
-        nums.remove(nums[-1] )
+        end -=1
+       
 
 print(cnt)
 
