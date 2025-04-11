a686381adc50877eaffb79f3dea5ba46e4ec4c05
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-04-11 10:29:20 +0900

    Title: 점 빼기, Time: 767ms, Memory: 34MB, Status: Runtime Error - Codetree

---
+++ 250411/점 빼기/remove-point.py
@@ -0,0 +1,21 @@
+from sortedcontainers import SortedSet
+ 
+n,m = map(int,input().split())
+
+
+coordinate = [tuple(map(int,input().split())) for _ in range(n)]
+
+s = SortedSet(coordinate)
+
+
+for _ in range(n) : 
+    num = int(input())
+    idx = s.bisect_left((num,0))
+
+    if idx == len(s) : 
+        print(-1,-1)
+        continue
+
+    print(s[idx][0], s[idx][1])
+    s.remove(s[idx])
+    
