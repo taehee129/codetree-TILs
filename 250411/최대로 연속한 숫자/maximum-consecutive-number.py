62745822e13feed46dcbef72292729d628a40fbb
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-04-11 14:54:58 +0900

    Title: 최대로 연속한 숫자, Time: 1489ms, Memory: 42MB, Status: Wrong Answer - Codetree

--- 250411/최대로 연속한 숫자/maximum-consecutive-number.py
+++ 250411/최대로 연속한 숫자/maximum-consecutive-number.py
@@ -7,20 +7,22 @@ s = SortedSet()
 s.add((0,n))
 
 sum_set = SortedSet()
-sum_set.add(n-0+1)
+sum_set.add((n-0+1,0,n))
 for num in nums :
+
     idx = s.bisect_right((num,0))
     a,b = s[idx-1]
     
     s.remove((a,b))
-    sum_set.remove(b-a+1)
+    sum_set.remove((b-a+1,a,b))
     if a <= num-1 :
         s.add((a,num-1))
-        sum_set.add(num-1-a+1)
+        sum_set.add((num-1-a+1,a,num-1))
     if b>= num+1 :
         s.add((num+1,b))
-        sum_set.add(b-num-1+1)
+        sum_set.add((b-num-1+1,num+1,b))
     
-    print(sum_set[-1])
+    
+    print(sum_set[-1][0])
     
     
