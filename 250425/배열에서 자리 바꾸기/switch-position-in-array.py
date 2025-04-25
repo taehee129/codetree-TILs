18db831ebe321d4fc64de33e0c0359f1cdd77879
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-04-25 16:21:10 +0900

    Title: 배열에서 자리 바꾸기, Time: 85ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250425/배열에서 자리 바꾸기/switch-position-in-array.py
+++ 250425/배열에서 자리 바꾸기/switch-position-in-array.py
@@ -17,9 +17,31 @@ for i in range(n) :
 
     if i !=0 : 
         prev_node = linked[i-1] 
-        prev_node.next = node
+        prev_node.next = node                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
         node.prev = prev_node
 
+def pop(node1,node2) : 
+    if node1.prev is not None :
+        node1.prev.next = node2.next 
+    if node2.next is not None :
+        node2.next.prev = node1.prev
+
+def link(node1,node2) :
+    node1.next = node2
+    node2.prev = node1
+
+def insert_prev(node1,node2) :
+    link(node2,node1)
+    if node1.prev is not None :
+        link(node1.prev, node2)
+
+def insert_next(node1,node2) : 
+    link(node1,node2)
+    if node1.next is not None :
+        link(node2,node1.next)
+
+def swap(a,b,c,d) : 
+    pass
 
 for _ in range(q) :
     a,b,c,d = map(int,input().split())
@@ -29,27 +51,73 @@ for _ in range(q) :
     c = linked[c-1]
     d = linked[d-1]
 
-    prev_a = a.prev 
-    next_b = b.next 
-    prev_c = c.prev
-    next_d = d.next
-    
-    if prev_a is not None :
-        prev_a.next = c
-    c.prev = prev_a
-    if next_b is not None :
-        next_b.prev = d 
-    d.next = next_b
+    if (b.next is not None and b.next.data == c.data)  :
+        c_prev = c.prev 
+        d_next = d.next
 
-    if prev_c is not None : 
-        prev_c.next =a 
-    a.prev = prev_c
-    if next_d is not None :
-        next_d.prev = b 
-    b.next = next_d
-    for node in linked :
-        print(node.prev.data if node.prev is not None else None, node.data , node.next.data if node.next is not None else None)
-        # if node.prev is None :
-        #     head = node
-    print('*'*50)
+        c.prev = a.prev
+        if a.prev is not None :
+            a.prev.next =c 
+        
+        d.next = a 
+        a.prev = d
+        b.next = d_next 
+        if d_next is not None :
+            d_next.prev = b
+        
+        
+    elif (d.next is not None and d.next.data == a.data):
+        a.prev = c.prev
+        if c.prev is not None :
+            c.prev.next =a 
+        
+        b.next = c 
+        c.prev = b 
+
+        d.next = b.next 
+        if b.next is not None :
+            b.next.prev = d
+                   
+    else : 
+        a_prev = a.prev
+        b.next = b.next
+        c_prev = c.prev
+        d_next = d.next 
+
+        c.prev = a.prev 
+        d.next = b.next 
+        
+        if a.prev is not None :
+            a.prev.next = c
+        if b.next is not None :
+            b.next.prev = d
+
+        a.prev = c_prev 
+        b.next = d_next 
+
+        if c_prev is not None :
+            c_prev.next = a
+        if d_next is not None :
+            d_next.prev = b 
+
+    # for node in linked :
+    #     print(node.prev.data if node.prev is not None else None, node.data,node.next.data if node.next is not None else None)
+    # print('*'*50)
+
+
+for node in linked :
+    if node.prev is None :
+        head = node
+
+node = head
+for i in range(n) :
+    print(node.data, end=' ')
+    node = node.next
+    
+
+
+    
+    
+
+   
 
