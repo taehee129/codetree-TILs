c7c4e6b5b7a1c8a8bc1cea7928485bff672c709c
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-06-11 10:49:16 +0900

    Title: 숫자 빠르게 찾기, Time: 357ms, Memory: 23MB, Status: Passed - Codetree

--- 250611/숫자 빠르게 찾기/find-number-fast.py
+++ 250611/숫자 빠르게 찾기/find-number-fast.py
@@ -4,14 +4,25 @@ n,m = map(int,input().split())
 
 nums = list(map(int,input().split()))
 
-for i in range(n) : 
-    num_dict[nums[i]] = i+1 
+
+def binary_search(target) : 
+    left= 0
+    right = n-1 
+
+    while (left <= right) : 
+        mid = (left+right) // 2 
+        
+        if nums[mid] == target :
+            return mid+1 
+        elif nums[mid] < target : 
+            left =mid +1
+        elif nums[mid] > target :
+            right = mid-1 
+        
+    return -1 
 
 
 for _ in range(m) : 
-    num = int(input())
-    
-    if num in num_dict : 
-        print(num_dict[num])
-    else :
-        print(-1)
+    target = int(input())
+
+    print(binary_search(target))
