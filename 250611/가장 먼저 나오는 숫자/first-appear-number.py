db8d285788eb2f774080ee1e64c312589cdd9ebd
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-06-11 11:51:17 +0900

    Title: 가장 먼저 나오는 숫자, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250611/가장 먼저 나오는 숫자/first-appear-number.py
@@ -0,0 +1,30 @@
+n,m = map(int,input().split())
+
+nums = list(map(int,input().split()))
+
+def upper_bound(target) :
+    left = 0
+    right=n-1 
+    min_idx = n 
+
+    while left <= right :
+        mid = (left + right) //2 
+
+        if nums[mid] >= target : 
+            min_idx = min(mid,min_idx) 
+            right = mid -1 
+        else : 
+            left = mid +1 
+    
+    return min_idx 
+
+questions = list(map(int,input().split()))
+
+
+for num in questions : 
+    ans = upper_bound(num) 
+
+    if num != nums[ans]:
+        print(-1) 
+    else : 
+        print(ans+1)
