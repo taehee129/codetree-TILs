8d4dab2e89411d0349ae173623a35296c29bf352
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-06-13 13:55:08 +0900

    Title: 삼 오 무, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250613/삼 오 무/three-five-moo.py
+++ 250613/삼 오 무/three-five-moo.py
@@ -1,4 +1,4 @@
-MAX_NUM =10**9
+MAX_NUM =10**10
 
 n = int(input())
 
@@ -9,7 +9,7 @@ ans = 0
 while left <= right : 
     mid = (left+right)//2
     val = mid-(mid//3)-(mid//5)+(mid//15)
-
+    #print(val, mid, left, right)
     if val == n : 
         ans = mid   
         if ans%3==0 or ans%5==0 : 
