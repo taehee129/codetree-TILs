812a1c6e18dd885620c0a4bb3a442cf0f98bf71f
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-06-16 12:20:35 +0900

    Title: 겹치지 않는 선분위의 두 점, Time: 980ms, Memory: 30MB, Status: Passed - Codetree

--- 250616/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.py
+++ 250616/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.py
@@ -4,25 +4,27 @@ lines = [tuple(map(int,input().split())) for _ in range(m)]
 
 
 lines.sort(key=lambda x:x[0])
-
-
-
 def check(val) :
     
     point= lines[0][0]
-    cnt = 1
+    cnt = 0
     for i,(a,b) in enumerate(lines) : 
         point = a if point < a else point       
         if point>b :
             continue 
+        
+        sum_val = (b-point)//val +1 
         cnt += (b-point)//val +1 
-        point = b+val 
+        #print(point,b,cnt)
+        point += sum_val *val
+
         if cnt >=n :
             return True 
-    
+    #print(cnt)
     return False 
                
-    
+#check(2)
+
 left =1
 right = 10**18
 ans = 0
