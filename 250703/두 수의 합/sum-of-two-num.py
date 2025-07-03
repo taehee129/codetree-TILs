b592e74286c8aa9e33148f18165a3272fcf28a9f
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-07-03 11:25:52 +0900

    Title: 두 수의 합, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250703/두 수의 합/sum-of-two-num.py
@@ -0,0 +1,23 @@
+n,k = map(int,input().split())
+
+nums = list(map(int,input().split()))
+
+
+d ={}
+
+for num in nums :
+    if num in d : 
+        d[num] +=1 
+    else :
+        d[num] =1 
+
+cnt =0
+for num in nums : 
+    diff = k-num 
+    if diff in d :
+        cnt += d[diff]
+    if diff == k :
+        cnt -= 1 
+    
+print(cnt//2)
+    
