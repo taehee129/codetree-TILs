c3d11f813da46b9b213591a056ba4aee7384dc0c
taehee129 <89380104+taehee129@users.noreply.github.com>
2025-09-16 13:51:45 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 194ms, Memory: 34MB, Status: Passed - Codetree

--- 250916/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
+++ 250916/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
@@ -7,12 +7,14 @@ ranges = [tuple(map(int,input().split())) for _ in range(q)]
 prefix_sum = [0]*(10**6)
 
 for dot in dots : 
-    prefix_sum[dot-1] = 1 
+    prefix_sum[dot] = 1 
 
 for i in range(1,10**6) : 
     prefix_sum[i] = prefix_sum[i-1] + prefix_sum[i]
 
 for i in range(q) : 
-    x,y = ranges[i]
-    #print(prefix_sum[y-1],prefix_sum[x-2])
-    print(prefix_sum[y-1]-prefix_sum[x-2])
+    x,y = ranges[i]   
+    if x == 0 :
+        print(prefix_sum[y])
+    else :
+        print(prefix_sum[y]-(prefix_sum[x-1]))
